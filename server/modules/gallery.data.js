55ee05dc0d4089b125a290ed0acb9ca562fb8fef
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-22 15:38:38 -0500

    fixes mock data ids - unique

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -6,19 +6,19 @@ const galleryItems = [
     likes: 0,
   },
   {
-    id: 1,
+    id: 2,
     path: 'images/goat_small.jpg',
     description: 'Photo of a goat taken at Glacier National Park.',
     likes: 0,
   },
   {
-    id: 1,
+    id: 3,
     path: 'images/goat_small.jpg',
     description: 'Photo of a goat taken at Glacier National Park.',
     likes: 0,
   },
   {
-    id: 1,
+    id: 4,
     path: 'images/goat_small.jpg',
     description: 'Photo of a goat taken at Glacier National Park.',
     likes: 0,
