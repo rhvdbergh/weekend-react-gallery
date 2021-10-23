bea61f76553fc04ad08a830270848e54eb6dfc04
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-23 08:46:36 -0500

    adds html form

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -1,7 +1,23 @@
 function GalleryForm({ fetchGalleryItems }) {
+  const addPhoto = (event) => {
+    event.preventDefault();
+    console.log(`About to add a photo!`);
+  };
+
   return (
     <div className="galleryForm">
-      <p>GalleryForm placeholder</p>
+      <form action="">
+        <p>GalleryForm placeholder</p>
+        <label htmlFor="photoPath">Path to photo</label>
+        <input type="text" id="photoPath" placeholder="http://path_to_pic..." />
+        <label htmlFor="photoDescription">Description</label>
+        <input
+          type="text"
+          id="photoDescription"
+          placeholder="That time when..."
+        />
+        <button onClick={addPhoto}>Add Photo</button>
+      </form>
     </div>
   );
 }
